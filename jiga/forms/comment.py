66fedea37fcb636af441595d034a49122baf1352
mixser <mixser.by@gmail.com>
2014-02-03 21:30:49 +0300

    Set comment_text widget to forms.Textarea

--- jiga/forms/comment.py
+++ jiga/forms/comment.py
@@ -5,4 +5,4 @@ __author__ = 'mixser'
 
 class CommentForm(forms.Form):
     comment_title = forms.CharField()
-    comment_text = forms.CharField()
+    comment_text = forms.CharField(widget=forms.Textarea)
