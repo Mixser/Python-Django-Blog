4456c762818bea4263565e2f9f8b2b6cb07e0989
mixser <mixser.by@gmail.com>
2014-02-05 18:52:56 +0300

    Add url to follow and unfollow

--- jiga/urls.py
+++ jiga/urls.py
@@ -1,3 +1,4 @@
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -12,10 +13,13 @@ urlpatterns = patterns('',
     url(r'^post/(?P<post_id>\d+)$', views.post, name='post'),
     url(r'^post/(?P<post_id>\d+)/edit$', views.edit_post, name='edit_post'),
     url(r'^profile/(?P<user_id>\d+)$', views.profile, name='profile'),
+    url(r'^profile/(?P<user_id>\d+)/follow$', views.follow, name='follow'),
+    url(r'^profile/(?P<user_id>\d+)/unfollow$', views.unfollow, name='unfollow'),
     url(r'^register/', views.register, name='register'),
     url(r'^login/', views.login, name='login'),
     url(r'^logout/', views.logout, name='logout'),
     url(r'^new_post/', views.create_post, name='create_post'),
     url(r'^profile/edit', views.edit_profile, name='edit_profile'),
     url(r'^create_comment/(?P<post_id>\d+)$', views.create_comment, name='create_comment')
+
 )
