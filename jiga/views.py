0d82cae1412462aa7db6acea6441bc8c9798bc7f
mixser <mixser.by@gmail.com>
2014-02-05 18:53:58 +0300

    Refactoring

--- jiga/views.py
+++ jiga/views.py
@@ -13,7 +13,7 @@ from jiga.forms.edit_user_profile import EditProfileForm
 from jiga.forms.login import LoginForm
 from jiga.forms.post import PostForm
 from jiga.forms.registration import RegistrationForm
-from jiga.models import Post, Comment
+from jiga.models import Post, Comment, Relationship
 
 
 def index(request):
@@ -22,27 +22,27 @@ def index(request):
 
 
 def post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+    post_obj = get_object_or_404(Post, pk=post_id)
+    title = post_obj.post_title
     form = CommentForm()
-    return render(request, 'jiga/post.html', {'post': post, 'form': form})
+    return render(request, 'jiga/post.html', {'post': post_obj, 'form': form, 'title': title})
 
 
 @login_required
 def edit_post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    if post.user != request.user:
+    post_obj = get_object_or_404(Post, pk=post_id)
+    if post_obj.user != request.user:
         return HttpResponseRedirect(reverse('jiga:index'))
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            post.post_title = form.cleaned_data['post_title']
-            post.post_text = form.cleaned_data['post_text']
-            post.pub_date = form.cleaned_data['pub_date']
-            post.save()
-            return HttpResponseRedirect(reverse('jiga:post', args=(post.id,)))
-
+            post_obj.post_title = form.cleaned_data['post_title']
+            post_obj.post_text = form.cleaned_data['post_text']
+            post_obj.pub_date = form.cleaned_data['pub_date']
+            post_obj.save()
+            return HttpResponseRedirect(reverse('jiga:post', args=(post_obj.id,)))
     else:
-        form = PostForm(post.get_dict())
+        form = PostForm(post_obj.get_dict())
         form['pub_date'].css_classes('datepicker')
     return render(request, 'jiga/post_form.html', {'form': form})
 
@@ -68,14 +68,14 @@ def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
             user = authenticate(username=username, password=password)
             if user is not None and user.is_active:
                 _login(request, user)
                 return HttpResponseRedirect(reverse('jiga:profile', args=(user.id,)))
             else:
-                    return render(request, 'jiga/login.html', {'form': form})
+                return render(request, 'jiga/login.html', {'form': form})
     else:
         form = LoginForm()
     return render(request, 'jiga/login.html', {'form': form})
@@ -99,7 +99,7 @@ def register(request):
                         first_name=first_name, last_name=last_name)
             user.set_password(password)
             try:
-                user.save()   # TODO: validate for multiply user
+                user.save()   # TODO: validate for multiply email
             except IntegrityError:
                 return render(request, 'jiga/registration.html',
                               {'form': form, 'error_message': 'A user with this name already exists'})
@@ -111,11 +111,21 @@ def register(request):
 
 def profile(request, user_id):
     user = get_object_or_404(User, pk=user_id)
+    followed = None
     if user == request.user:
         posts = user.post_set.all().order_by('-pub_date')
     else:
+        followed = following(request.user, user)
         posts = user.post_set.all().filter(pub_date__lte=timezone.now()).order_by('-pub_date')
-    return render(request, 'jiga/profile.html', {'user_for_viewing': user, 'posts': posts})
+    list_recently_comments = user.comment_set.all().order_by('-pub_date')[:5]
+    list_followed_user = user.from_user.all()
+    list_follower_user = user.to_user.all()
+    return render(request, 'jiga/profile.html', {'user_for_viewing': user, 'posts': posts,
+                                                 'list_recently_comments': list_recently_comments,
+                                                 'list_followed_user': list_followed_user,
+                                                 'list_follower_user': list_follower_user,
+                                                 'followed': followed })
+
 
 @login_required
 def edit_profile(request):
@@ -134,10 +144,9 @@ def edit_profile(request):
     return render(request, 'jiga/edit_user_profile.html', {'form': form})
 
 
-
 @login_required
 def create_comment(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+    post_obj = get_object_or_404(Post, pk=post_id)
     user = request.user
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -145,6 +154,33 @@ def create_comment(request, post_id):
             comment_title = form.cleaned_data['comment_title']
             comment_text = form.cleaned_data['comment_text']
             comment = Comment(comment_title=comment_title, comment_text=comment_text,
-                              user=user, post=post,pub_date=timezone.now())
+                              user=user, post=post_obj, pub_date=timezone.now())
             comment.save()
-    return HttpResponseRedirect(reverse('jiga:post', args=(post.id, ) ))
+    return HttpResponseRedirect(reverse('jiga:post', args=(post_obj.id, )))
+
+
+@login_required
+def follow(request, user_id):
+    from_user = request.user
+    to_user = get_object_or_404(User, pk=user_id)
+    if to_user != from_user:
+        new_relationship = Relationship(from_user=from_user, to_user=to_user)
+        new_relationship.save()
+        return HttpResponseRedirect(reverse('jiga:profile', args=(user_id, )))
+    return HttpResponseRedirect(reverse('jiga:profile', args=(user_id, )))
+
+
+def unfollow(request, user_id):
+    from_user = request.user
+    res = Relationship.objects.filter(from_user__id=from_user.id,
+                                      to_user__id=user_id)
+    if res is not None:
+        res.delete()
+    return HttpResponseRedirect(reverse('jiga:profile', args=(user_id, )))
+
+
+def following(from_user, to_user):
+    if Relationship.objects.filter(from_user__id=from_user.id,
+                                   to_user__id=to_user.id):
+        return True
+    return False
